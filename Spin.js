c01ea1ff757559ec9d927904865bf1f1b886013a
akdo-bro-2 <121363329+akdo-bro-2@users.noreply.github.com>
2024-01-31 22:28:39 -0500

    Update Spin.js

--- Spin.js
+++ Spin.js
@@ -1,3 +1,5 @@
-document.querySelector("img").addEventListener("click", function() {
-  this.style.animation = "spin 1s linear infinite";
+document.getElementById("myButton").addEventListener("click", function() {
+  for (let i = 0; i < 10; i++) {
+    document.getElementById("myButton").style.transform = `rotate(${i * 36}deg)`;
+  }
 });
